bc724621d456348ba4601e368b33f68b8e0d7bcc
Anthony Moss <asmoss93@gmail.com>
2019-06-24 17:40:48 -0400

    adds function that takes array of number and returns sum of the positive numbers

---
+++ sumOfPositive.js
@@ -0,0 +1,31 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+    // sets array that will hold all pos numbers
+    let resultArray = []
+    // loops through arr to get all pos numbers
+    for (i=0;i<=arr.length-1;i++){
+        // if the number in the array is greater than or = 0
+      if (arr[i] >= 0) {
+        //   it gets pushed tothe results array holding our positive numbers
+        resultArray.push(arr[i])
+      }
+    //   no need for an else bc neg numbers are disregarded
+    }
+    // if results array isnt empty
+    if (resultArray.length) {
+        // retrun the result of the reduce method on resultsArray with the get sum function, which takes a total and a number
+      return (resultArray.reduce(function getSum(total, num) {
+        //   getSum just take the current total and adds the number and returns it
+        // this will happen until there is just one number left in array so its acting to add all numbers of our array together
+        return total + num;
+      }))
+    //   if the results array is empty, so no pos numbers we retun 0
+    } else {
+      return 0
+    }
+  }
