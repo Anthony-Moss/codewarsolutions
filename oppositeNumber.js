1df973373e4fa1c188177e2d3157461add13407e
Anthony Moss <asmoss93@gmail.com>
2019-06-25 16:20:40 -0400

    simple function thatreturns the opposite or negative of input number

---
+++ oppositeNumber.js
@@ -0,0 +1,8 @@
+// given an input number, return the opposite value 
+// ie input = 1, output = -1,
+//    input = -25, output = 25
+//    input = 259892, output = -259892
+
+function opposite(number) {
+    return -(number)
+}
