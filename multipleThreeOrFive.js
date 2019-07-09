1c28f9957dc500b14764041b08ca194ac7a4a51d
Anthony Moss <asmoss93@gmail.com>
2019-07-09 08:30:20 -0400

    adds much simpler solution, removing comlpetely useless array in my first solution

--- multipleThreeOrFive.js
+++ multipleThreeOrFive.js
@@ -39,3 +39,13 @@ function solution(number){
         return 0
     }
 }
+
+function solutionEasy(number) {
+    let total = 0;
+    for (i=1; i<number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            total += i
+        }
+    }
+    return total
+}
