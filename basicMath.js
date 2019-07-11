bbcb4f27c08dd9dd6e5020c2b7b108e43e421df5
Anthony Moss <asmoss93@gmail.com>
2019-07-11 18:27:53 -0400

    adds default case

--- basicMath.js
+++ basicMath.js
@@ -20,5 +20,7 @@ function basicOp(operation, value1, value2){
             return (value1*value2)
         case '/':
             return (value1/value2)
+        default:
+            return 0
     }
 }
