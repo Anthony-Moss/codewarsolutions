669459565895794d232eb82c341f52e4737dc7fc
Anthony Moss <asmoss93@gmail.com>
2019-10-14 16:45:36 -0400

    adds first attempt python solution to reverse words 8 kyu kata

--- Python/reverse_words.py
+++ Python/reverse_words.py
@@ -1,3 +1,9 @@
+# Complete the solution so that it reverses all of the words within the string passed in.
 # reverseWords("The greatest victory is that which requires no battle")
 # // should return "battle no requires which that is victory greatest The"
 
+def reverseWords(str):
+    words = str.split(" ")
+    words.reverse()
+    result = ' '.join(words)
+    return result
